d7ace7ad0d3edecd0100c2868b91c2afcd57ab41
limagln <gabriellneiva@outlook.com>
2024-11-06 23:23:14 -0300

    Add Menu Inicial

--- GestaoAcademica/Program.cs
+++ GestaoAcademica/Program.cs
@@ -7,22 +7,56 @@ namespace GestaoAcademica
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Seja bem-vindo(a). \n" +
-        "Digite se você é aluno(a) ou professor(a): \n" +
-        "1 - Aluno(a). \n" +
-        "2 - Professor(a). ");
-      int permissao = Convert.ToInt32(Console.ReadLine());
-      
+      int permissao = 0;
+      while (permissao == 0)
+      {
+        Console.WriteLine("Seja bem-vindo(a). \n" +
+          "Digite a opção se você é aluno(a) ou professor(a): ");
+        Console.WriteLine("1 - Aluno(a).");
+        Console.WriteLine("2 - Professor(a).");
+        Console.WriteLine("3 - Sair.");
+        permissao = Convert.ToInt32(Console.ReadLine());
+
+        switch (permissao)
+        {
+          case 1:
+            IniciarAluno();
+            break;
+
+          case 2:
+            IniciarProfessor();
+            break;
+
+          case 3:
+            Console.WriteLine("Finalizando o sistema.");
+            return;
+
+          default:
+            Console.WriteLine("Opção inválida.");
+            permissao = 0;
+            break;
+        }
+      }
     }
 
     public static void IniciarAluno()
     {
+      Console.Clear();
+      Console.WriteLine("Aluno.");
 
+      //Implementar metodos permitidos aos alunos
+
+      //Opcao voltar ao menu
     }
 
     public static void IniciarProfessor()
     {
+      Console.Clear();
+      Console.WriteLine("Professor.");
 
+      //Implementar metodos permitidos aos professores
+
+      //Opcao voltar ao menu
     }
   }
 }
