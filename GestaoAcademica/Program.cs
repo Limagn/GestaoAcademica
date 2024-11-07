9e4793675bcda79621ab4ddefbac972fc8d1da5c
limagln <gabriellneiva@outlook.com>
2024-11-06 22:51:34 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoAcademica/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GestaoAcademica.Models;
+
+namespace GestaoAcademica
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Seja bem-vindo(a). \n" +
+        "Digite se você é aluno(a) ou professor(a): \n" +
+        "1 - Aluno(a). \n" +
+        "2 - Professor(a). ");
+      int permissao = Convert.ToInt32(Console.ReadLine());
+      
+    }
+
+    public static void IniciarAluno()
+    {
+
+    }
+
+    public static void IniciarProfessor()
+    {
+
+    }
+  }
+}
