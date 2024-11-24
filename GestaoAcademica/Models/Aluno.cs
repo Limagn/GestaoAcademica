42ffbc9f57572d5399d8ada14da25987fb5d440d
limagln <gabriellneiva@outlook.com>
2024-11-24 16:24:02 -0300

    Add Aluno e Professor

--- GestaoAcademica/Models/Aluno.cs
+++ GestaoAcademica/Models/Aluno.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace GestaoAcademica.Models
 {
-  public class Aluno
+  public class Aluno : Pessoa, IAvaliacao
   {
+    public Aluno(string nome, string sobrenome, string matricula) : base(nome, sobrenome, matricula)
+    {
+    }
+
+    public List<Professor> Professores { get; set; }
+
+    public override void Apresentacao()
+    {
+      if (Idade != 0)
+      {
+        Console.WriteLine($"Olá! Meu nome é {NomeCompleto} e tenho {Idade} anos. Sou aluno na UniFOA e tenho aula de:");
+      }
+      else
+      {
+        Console.WriteLine($"Olá! Meu nome é {NomeCompleto}. Sou professor na UniFOA e tenho aula de:");
+      }
+    }
+    public void Avaliar(string professor, int nota)
+    {
+      Console.WriteLine($"O aluno {NomeCompleto} avaliou o {professor} com a nota {nota}.");
+    }
+    public override string ToString()
+    {
+      return $"Nome: {Nome} {Sobrenome}, Matrícula: {Matricula}";
+    }
 
   }
 }
--- GestaoAcademica/Models/Professor.cs
+++ GestaoAcademica/Models/Professor.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace GestaoAcademica.Models
 {
-  public class Professor 
+  public class Professor : Pessoa, IAvaliacao
   {
-    
+    public Professor(string nome, string sobrenome, string matricula) : base(nome, sobrenome, matricula)
+    {
+    }
+
+    public override void Apresentacao()
+    {
+      if (Idade != 0)
+      {
+        Console.WriteLine($"Olá! Meu nome é {NomeCompleto} e tenho {Idade} anos. Sou professor na UniFOA e dou aula de: ");
+      }
+      else
+      {
+        Console.WriteLine($"Olá! Meu nome é {NomeCompleto}. Sou professor na UniFOA e dou aula de: ");
+      }
+    }
+    public void Avaliar(string aluno, int nota)
+    {
+      var alunos = BancoDeDados.DictAlunos.Values;
+      Console.WriteLine($"O professor {NomeCompleto} avaliou o {aluno} com a nota {nota}.");
+    }
+    public override string ToString()
+    {
+      return $"Nome: {Nome} {Sobrenome}, Matrícula: {Matricula}";
+    }
   }
 }
